827330bd6ab9823a662dcb2f74b63578d7ed41c8
Leonardo Aureliano <55006042+leoaureliano@users.noreply.github.com>
2022-06-09 18:09:09 -0700

    Add files via upload

---
+++ W5/CalendarMenu.cpp
@@ -0,0 +1,26 @@
+#iinclude <iostream>
+
+using namespace std;
+
+void f1() {
+    cout << ""
+}
+
+
+
+
+int main (){
+    
+    string calendar;
+    int year, day;
+
+    cout << "Enter the name of the output file: "\n;
+    cin >> calendar; 
+    cout << "Enter the year fir which you wish to generate the calendar: \n";
+    cin >> year;
+    cout << "Enter the day of the week that: \n";
+    cin >> day; 
+
+    f1();
+
+}
